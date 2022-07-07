79857e666a28134bda350710620cd1feaf7c3b8d
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-07-07 17:10:14 -0300

    Primeiro commit

---
+++ src/main/java/com/listamercado/ListaDeMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.listamercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeMercadoApplication.class, args);
+	}
+
+}
