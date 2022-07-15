8e080e0a3a4d7c652dae4caa347dfd73a151ce43
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-07-15 19:26:42 -0300

    Adiiconado um arquivo de população do DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_marca(nome) VALUES("Urca");
+INSERT INTO tb_marca(nome) VALUES("Dona Benta");
+INSERT INTO tb_marca(nome) VALUES("Pomarolla");
+
+INSERT INTO tb_produto(descricao, nome, preco, marca_id) VALUES("Teste descrição","Desinfetante", 100, 1);
+INSERT INTO tb_produto(descricao, nome, preco, marca_id) VALUES("Teste descrição","Macarrão espaguete", 100, 2);
+INSERT INTO tb_produto(descricao, nome, preco, marca_id) VALUES("Teste descrição","Molho de tomate", 100, 3);
