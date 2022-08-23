540f9ba3eef41e79d92816a75b2da3a985e49445
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-08-23 17:29:00 -0300

    Correção

--- src/main/java/com/listamercado/service/MarcaService.java
+++ src/main/java/com/listamercado/service/MarcaService.java
@@ -1,14 +1,10 @@
 package com.listamercado.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.listamercado.repository.MarcaRepository;
 
 @Service
 public class MarcaService {
-    // @Autowired
-    // private MarcaRepository marcaRepository;
+
 
     
 }
